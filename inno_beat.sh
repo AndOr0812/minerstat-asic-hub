05a89cd908d3046a65310f113197f6b5e5e85b2b
coinscrow <hello@coinscrow.io>
2019-02-08 17:58:30 +0100

    Watchdog for all

--- inno_beat.sh
+++ inno_beat.sh
@@ -26,6 +26,16 @@ if screen -list | grep -q "ms-run" && ! screen -list | grep -q "minerstat"; then
   exit
 fi
 
+# ONLY MINERSTAT NO MS-RUN
+if ! screen -list | grep -q "ms-run" && screen -list | grep -q "minerstat"; then
+  echo "Frozen, restart"
+  screen -S minerstat -X quit # kill running process
+  screen -S ms-run -X quit # kill running process
+  screen -wipe &> /dev/null
+  screen -A -m -d -S minerstat sh /config/minerstat/minerstat.sh
+  exit
+fi
+
 # ONLY MINERSTAT
 if screen -list | grep -q "ms-run" && ! screen -list | grep -q "minerstat"; then
   echo "Frozen, restart (probably was fine, better to be secure)"
