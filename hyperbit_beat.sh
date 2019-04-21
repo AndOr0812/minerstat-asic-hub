eca768b99b364807cedbccabe28ccc13ead73150
coinscrow <hello@coinscrow.io>
2019-04-21 17:13:59 +0200

    Create hyperbit_beat.sh

---
+++ hyperbit_beat.sh
@@ -0,0 +1,22 @@
+#!/bin/sh
+
+# CURRENTLY ONLY USED FOR HYPERBIT TO MAINTAIN UPTIME AND STABILITY
+
+check() {
+
+  sleep 20
+
+  CHECKHEALTH=$(ps | grep -c minerstat.sh)
+
+  if [ "$CHECKHEALTH" != "1" ]
+  then
+  	echo ""
+  else
+  	nohup /bin/sh /usr/app/minerstat/minerstat.sh &
+  fi
+
+  check
+
+}
+
+check
